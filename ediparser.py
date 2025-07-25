f6893587028d721939eeec63304049b199aa9ef0
kenyash <lfenn3@wgu.edu>
2025-07-25 10:14:48 -0500

    practice file, code file

---
+++ ediparser.py
@@ -0,0 +1,23 @@
+##this program parses throuh the edi
+### this file is for personal use so i hard coded some aspects
+### however for a 
+
+
+def open_file_edi():
+    """opens sample edi file with exception thrown if no file found"""
+    with open('sf1.rtf', 'r') as edifile: #opening edi file
+        edicontents = edifile.read() #saving contents to a list for parsing
+    return edicontents
+
+
+def remove_chars(file):
+    chars_to_r = ["\n","~", "\\"]
+    for i in range(len(file)):
+        if file[i] in chars_to_r:
+            file = file.replace(file[i], " ")
+    return file 
+
+            
+
+        
+    
